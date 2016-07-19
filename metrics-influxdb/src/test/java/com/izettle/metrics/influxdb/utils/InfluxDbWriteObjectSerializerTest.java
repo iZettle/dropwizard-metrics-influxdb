5c877ef523fbbd3a148524b3c49636238dc3f6bd
Michal Matloka <mmatloka@gmail.com>
2016-07-19 14:42:36 +0200

    Ommit fields with values Double NaN's, +Inf, -Inf in line protocol (#48)
    * Ommit fields with values Double NaN's, +Inf, -Inf in line protocol
    
    * Fixed build for jdk 7

--- metrics-influxdb/src/test/java/com/izettle/metrics/influxdb/utils/InfluxDbWriteObjectSerializerTest.java
+++ metrics-influxdb/src/test/java/com/izettle/metrics/influxdb/utils/InfluxDbWriteObjectSerializerTest.java
@@ -7,13 +7,14 @@ import com.izettle.metrics.influxdb.data.InfluxDbPoint;
 import com.izettle.metrics.influxdb.data.InfluxDbWriteObject;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 public class InfluxDbWriteObjectSerializerTest {
-    
+
     @Test
     public void shouldSerializeUsingLineProtocol() {
         Map<String, String> tags = new HashMap<String, String>();
@@ -36,5 +37,22 @@ public class InfluxDbWriteObjectSerializerTest {
         assertThat(lineString).isEqualTo(
             "measurement1,tag1Key=tag1Value field1Key=\"field1Value\" 456000\n"
                 + "measurement1,tag1Key=tag1Value field1Key=\"field1Value\" 456000\n");
+    }
+
+    @Test
+    public void shouldOmitNaNsEtcWhenSerializingUsingLineProtocol() {
+        Map<String, Object> fields = new LinkedHashMap<String, Object>();
+        fields.put("field1Key", "field1Value");
+        fields.put("field2Key", Double.NaN);
+        fields.put("field3Key", Double.POSITIVE_INFINITY);
+        fields.put("field4Key", Double.NEGATIVE_INFINITY);
+        fields.put("field5Key", 0.432);
+        InfluxDbWriteObject influxDbWriteObject = new InfluxDbWriteObject("test-db",TimeUnit.MICROSECONDS);
+        influxDbWriteObject.getPoints().add(new InfluxDbPoint("measurement1", 456l, fields));
+
+        InfluxDbWriteObjectSerializer influxDbWriteObjectSerializer = new InfluxDbWriteObjectSerializer();
+        String lineString = influxDbWriteObjectSerializer.getLineProtocolString(influxDbWriteObject);
+
+        assertThat(lineString).isEqualTo("measurement1 field1Key=\"field1Value\",field5Key=0.432 456000\n");
     }
 }
