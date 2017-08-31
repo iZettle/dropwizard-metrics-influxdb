54c76280e10ae04dfe53b09e64701f60aac1d8aa
John Wang <john@dashbase.io>
2017-08-31 00:52:07 -0700

    added logger sender

---
+++ metrics-influxdb/src/main/java/com/izettle/metrics/influxdb/InfluxDbLoggerSender.java
@@ -0,0 +1,21 @@
+package com.izettle.metrics.influxdb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.codec.Charsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfluxDbLoggerSender extends InfluxDbBaseSender {
+
+	private static final Logger logger = LoggerFactory.getLogger(InfluxDbLoggerSender.class);
+	public InfluxDbLoggerSender(String database, TimeUnit timePrecision, String measurementPrefix) {
+		super(database, timePrecision, measurementPrefix);
+	}
+
+	@Override
+	protected int writeData(byte[] line) throws Exception {
+		logger.info(new String(line, Charsets.UTF_8));
+		return 0;
+	}
+}
