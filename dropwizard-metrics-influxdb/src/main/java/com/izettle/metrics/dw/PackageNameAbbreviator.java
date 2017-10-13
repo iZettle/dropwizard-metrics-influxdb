70bc23faa8349390f849a7f589ffa91c7f7bb138
Henrik Stråth <henrik.strath@izettle.com>
2017-10-13 15:58:12 +0200

    Make Pattern static in PackageNameAbbreviator

--- dropwizard-metrics-influxdb/src/main/java/com/izettle/metrics/dw/PackageNameAbbreviator.java
+++ dropwizard-metrics-influxdb/src/main/java/com/izettle/metrics/dw/PackageNameAbbreviator.java
@@ -7,7 +7,7 @@ public class PackageNameAbbreviator {
     // Capture the first letter of all dot-separated words that comes before the first capitalized word. E.g:
     // Input:      org.example.MyClass.myMethod
     // Captures:   ^   ^
-    private Pattern pattern = Pattern.compile("([a-z])\\w+(?=.*\\.[A-Z])");
+    private static Pattern pattern = Pattern.compile("([a-z])\\w+(?=.*\\.[A-Z])");
 
     public String abbreviate(String fullyQualifiedName) {
         return pattern
